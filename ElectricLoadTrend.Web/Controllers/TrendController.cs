9a5a3acc9063a0b018019cc926f8b61ef97b43bf
InstAcntRP <168220592+InstAcntRP@users.noreply.github.com>
2024-06-15 13:06:39 -0400

    Added controller and swagger library

---
+++ ElectricLoadTrend.Web/Controllers/TrendController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectricLoadTrend.Web;
+
+[ApiController]
+[Route("[controller]")]
+public class TrendController : ControllerBase
+{
+     [HttpGet("[action]")]
+    public IActionResult GetCurrentTime()
+    {
+        string dt = string.Empty;
+        dt = DateTime.Now.ToString("MM-dd-yyyy hh:mm:ss");
+        return Ok(dt);
+    }
+}
